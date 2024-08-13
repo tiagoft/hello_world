674bbc2c45a05c4aa1bb69cd71b8e5eedb6cfc46
Tiago Tavares <tiagoft@gmail.com>
2024-08-13 14:53:51 -0300

    Pacote minimalista online

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Lendo o conteúdo do README.md para usar como descrição longa
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hello_world",  # Substitua pelo nome do seu pacote
+    version="0.1.0",
+    author="Tiago Tavares",
+    author_email="tiagoft@gmail.com",
+    description="Um pacote minimalista em python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tiagoft/hello_world",  # URL do repositório do seu projeto (se houver)
+    packages=find_packages(),  # Encontra automaticamente todos os pacotes no diretório
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+    entry_points={
+        'console_scripts': [
+            'tiago_hello_world=hello_world.main:main',
+        ],
+    },
+    install_requires=[  # Instala as dependências especificadas no requirements.txt
+        line.strip() for line in open("requirements.txt").readlines()
+    ],
+)
