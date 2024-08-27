a94bc1a1d6173097d25991c17fbe119cb738e78d
Joao-Gabriel05 <jgffaraco@gmail.com>
2024-08-27 11:59:55 -0300

    permutacao feita

--- hello_world/enigma.py
+++ hello_world/enigma.py
@@ -1,18 +1,27 @@
 import numpy as np
-from types import Tuple
+# from types import Tuple
 
-def gerar_matrizes_de_permutacao(N : int) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Gera duas matrizes de permutação de tamanho N x N.
-    """
-    raise NotImplementedError
+# def gerar_matrizes_de_permutacao(N : int) -> Tuple[np.ndarray, np.ndarray]:
+#     """
+#     Gera duas matrizes de permutação de tamanho N x N.
+#     """
+#     raise NotImplementedError
 
-def encriptar_enigma(mensagem : str,
-              P : np.ndarray,
-              Q : np.ndarray) -> str:
-    raise NotImplementedError
+# def encriptar_enigma(mensagem : str,
+#               P : np.ndarray,
+#               Q : np.ndarray) -> str:
+#     raise NotImplementedError
 
-def decriptar_enigma(mensagem_encriptada : str,
-              P : np.ndarray,
-              Q : np.ndarray) -> str:
-    raise NotImplementedError
+# def decriptar_enigma(mensagem_encriptada : str,
+#               P : np.ndarray,
+#               Q : np.ndarray) -> str:
+#     raise NotImplementedError
+
+def gerar_matrizes_de_permutacao(N):
+    matriz_identidade_mensagem = np.identity(N)
+    P = np.random.permutation(matriz_identidade_mensagem)
+    R = np.random.permutation(P)
+    
+    return P,R
+
+
