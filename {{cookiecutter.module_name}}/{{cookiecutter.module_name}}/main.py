759478f206574ed60f96e9b69410bccc55050b48
Tiago Tavares <tiagoft@gmail.com>
2024-09-05 11:44:11 -0300

    Version 1 of cookiecutter template

---
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/main.py
@@ -0,0 +1,34 @@
+import {{cookiecutter.module_name}}
+import os
+from pathlib import Path
+from rich.console import Console
+import typer
+
+app = typer.Typer()
+console = Console()
+
+@app.command('info')
+def print_info(custom_message : str = ""):
+    """
+    Print information about the module
+    """
+    console.print("Hello! I am {{cookiecutter.module_name}}")
+    console.print(f"Author: {{cookiecutter.module_name}}.__author__")
+    console.print(f"Version: {{cookiecutter.module_name}}.__version__")
+    if custom_message != "":
+        console.print(f"Custom message: {custom_message}")
+
+@app.command()
+def demo():
+    print("Hello world!")
+    {{cookiecutter.module_name}}.my_function()
+    script_path = Path(os.path.abspath(__file__))
+    parent_path = script_path.parent
+    print("Script path:", script_path)
+    with open(parent_path / "assets/poetry.txt") as f:
+        print(f.read())
+    with open(parent_path / "assets/test_folder/test_something.txt") as f:
+        print(f.read())
+
+if __name__ == "__main__":
+    app()
