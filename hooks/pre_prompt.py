e0ccb49dc34dcf05fa87c14d15d13702f7d9828a
Tiago Tavares <tiagoft@gmail.com>
2024-09-17 15:27:43 -0300

    Added pre_gen and pre_prompt hooks to check git status

---
+++ hooks/pre_prompt.py
@@ -0,0 +1,18 @@
+import sys
+import subprocess
+
+def is_git_installed() -> bool:
+    print("Checking if Git is installed...")
+    try:
+        subprocess.run(["git",  "--version"], capture_output=True, check=True)
+        return True
+    except Exception:
+        return False
+
+if __name__ == "__main__":
+    if not is_git_installed():
+        print("ERROR: Git is not installed. Use `apt install git` before proceeding!")
+        sys.exit(1)
+    
+    print("All requirements are met. You can proceed.")
+
