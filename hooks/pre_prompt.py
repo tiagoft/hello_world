c4623aeeb7ad2a09f56e2bb68772b88f8a822cc0
Tiago Tavares <tiagoft@gmail.com>
2024-09-17 15:37:35 -0300

    pre_prompt hook informs git is installed

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     if not is_git_installed():
         print("ERROR: Git is not installed. Use `apt install git` before proceeding!")
         sys.exit(1)
-    
+    else:
+        print("Git is installed.")
+    exit(1)
     print("All requirements are met. You can proceed.")
 
