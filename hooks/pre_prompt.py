ae64655421182c0a4f36adbcad58843d26dacd36
Tiago Tavares <tiagoft@gmail.com>
2024-09-17 16:28:16 -0300

    pre_prompt gets author name and e-mail from github configuration

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import json
 
 def is_git_installed() -> bool:
     print("Checking if Git is installed...")
@@ -8,6 +9,24 @@ def is_git_installed() -> bool:
         return True
     except Exception:
         return False
+    
+def get_git_user_name() -> str:
+    print("Getting Git user name...")
+    try:
+        result = subprocess.run(["git", "config", "--get", "user.name"], capture_output=True, check=True, text=True)
+        return result.stdout.strip()
+    except Exception as e:
+        print(f"ERROR: Unable to get Git user name. {e}")
+        return False
+
+def get_git_user_email() -> str:
+    print("Getting Git user email...")
+    try:
+        result = subprocess.run(["git", "config", "--get", "user.email"], capture_output=True, check=True, text=True)
+        return result.stdout.strip()
+    except Exception as e:
+        print(f"ERROR: Unable to get Git user email. {e}")
+        return False
 
 if __name__ == "__main__":
     if not is_git_installed():
@@ -15,6 +34,23 @@ if __name__ == "__main__":
         sys.exit(1)
     else:
         print("Git is installed.")
+
+    # Load cookiecutter.json
+    with open('cookiecutter.json', 'r') as file:
+        context = json.load(file)
+
+    git_user_name = get_git_user_name()
+    git_user_email = get_git_user_email()
+
+    new_context = {}
+
+    new_context['author_name'] = git_user_name
+    new_context['author_email'] = git_user_email
+
+    context.update(new_context)
+
+    with open('cookiecutter.json', 'w') as file:
+        json.dump(context, file, indent=4)
 
     print("All requirements are met. You can proceed.")
 
